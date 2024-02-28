978395b974f95ecdec330f8ff9088fca6d4ff4ad
Gabriel Castillo Serafim <castillo.serafim.gabriel@gmail.com>
2024-02-28 18:04:51 +0100

    Initial Commit

---
+++ GenericAlertPresenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericAlertPresenter
+//
+//  Created by Gabriel Castillo Serafim on 28/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
